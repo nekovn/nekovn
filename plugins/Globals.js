3c326513a2c1ffb5d1822df810f209b249fd2bc0
nekovn <77607294+nekovn@users.noreply.github.com>
2021-02-03 22:54:10 +0900

    Update Globals.js

--- plugins/Globals.js
+++ plugins/Globals.js
@@ -1,9 +1,22 @@
 import SiteConfig from '~/config/_siteConfig'
+import https from 'https'
 
 export default ({ $axios,app,env }, inject) => {
-  const api = $axios.create({});
-  const wpApi = $axios.create({});
-  const wpApiContact = $axios.create({});
+  const api = $axios.create({
+    httpsAgent : new https.Agent({
+      rejectUnauthorized : false
+    })
+  });
+  const wpApi = $axios.create({
+    httpsAgent : new https.Agent({
+      rejectUnauthorized : false
+    })
+  });
+  const wpApiContact = $axios.create({
+    httpsAgent : new https.Agent({
+      rejectUnauthorized : false
+    })
+  });
 
   api.setBaseURL(env.baseUrl+'/wp/v2');
   wpApi.setBaseURL(env.baseUrl);
