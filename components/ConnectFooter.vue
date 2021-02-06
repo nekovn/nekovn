b9b2b83abdca635466b86308eed07001e5c399cf
nekovn <77607294+nekovn@users.noreply.github.com>
2021-02-07 01:47:34 +0900

    Update ConnectFooter.vue

--- components/ConnectFooter.vue
+++ components/ConnectFooter.vue
@@ -2,6 +2,11 @@
   <div>
     <h3>公式ソーシャルアカウント</h3>
     <get-social/>
+    <div style="margin-top: 20px">
+      <a href="https://www.gogetssl.com" rel="nofollow" title="GoGetSSL Site Seal Logo">
+        <img src="https://gogetssl-cdn.s3.eu-central-1.amazonaws.com/site-seals/gogetssl-static-seal.svg" width="180" height="58" title="GoGetSSL Site Seal, Protected website" alt="GoGetSSL Site Seal"/>
+      </a>
+    </div>
   </div>
 </template>
 <script>
