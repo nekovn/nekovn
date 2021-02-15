2f2eb5f9f3db46d38c09f957026b47df9c5735ad
nekovn <77607294+nekovn@users.noreply.github.com>
2021-02-16 00:01:23 +0900

    Update head.js

--- config/head.js
+++ config/head.js
@@ -9,13 +9,15 @@ export default {
       content: process.env.npm_package_description || ''
     }
   ],
-  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    script: [
+  script: [
     {
-      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
       'data-ad-client': 'pub-1530282318991102',
-      async: true
+      async: true,
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+
     }
-  ]
+  ],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  
 
 }
