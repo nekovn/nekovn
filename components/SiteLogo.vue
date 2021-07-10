8da7c7bdead18a18f181d5770957906d101b5bf2
Rim <77607294+nekovn@users.noreply.github.com>
2021-07-10 18:49:19 +0900

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="logo-wrapper">
-    <div
-      :class="{
-        'logo-inner': true,
-        blink: blinking
-      }"
-    >
       <img
-        :key="x"
         :src="`/logo-frames/neko-vn.png`"
       />
     </div>
@@ -37,23 +30,11 @@ export default {
     transform: translateY(-360px);
   }
 }
-.logo-wrapper {
-  position: relative;
-  height: 50px;
-  overflow: hidden;
-  width: 200px;
-  .logo-inner {
-    position: absolute;
-    height: 500px;
-    img {
-      display: block;
-      height: 40px;
-      margin: 5px;
-      max-height: initial;
-    }
-    &.blink {
-      animation: logoBlink 0.2s steps(8, end) alternate;
-    }
+.logo-wrapper img{
+    display: block;
+    height: 128px;
+    width:auto;
+    margin: 5px;
   }
 }
 @media screen and (max-width: 575px) {
